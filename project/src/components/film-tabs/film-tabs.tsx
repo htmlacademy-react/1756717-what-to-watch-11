ffcd111a77adb0512ec71ae81db52592f22c6854
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-11-10 21:43:37 +0300

    Исправляет название переменной состояния в табах

--- project/src/components/film-tabs/film-tabs.tsx
+++ project/src/components/film-tabs/film-tabs.tsx
@@ -7,10 +7,10 @@ import FilmTabOverView from '../film-tab-overview/film-tab-overview';
 import FilmTabReviews from '../film-tab-reviews/film-tab-reviews';
 
 function FilmTabs({film, reviews}: FilmTabsProps): JSX.Element {
-  const [isActiveTab, setActiveTab] = useState<string>(TabValue.Overview);
+  const [activeTab, setActiveTab] = useState<string>(TabValue.Overview);
 
   const renderTab = () => {
-    switch (isActiveTab) {
+    switch (activeTab) {
       case TabValue.Overview:
         return <FilmTabOverView film={film} />;
       case TabValue.Details:
@@ -24,13 +24,13 @@ function FilmTabs({film, reviews}: FilmTabsProps): JSX.Element {
     <div className="film-card__desc">
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          <li className={`film-nav__item ${isActiveTab === TabValue.Overview ? 'film-nav__item--active' : ''}`} onClick={() => setActiveTab(TabValue.Overview)}>
+          <li className={`film-nav__item ${activeTab === TabValue.Overview ? 'film-nav__item--active' : ''}`} onClick={() => setActiveTab(TabValue.Overview)}>
             <Link to='#' className="film-nav__link">Overview</Link>
           </li>
-          <li className={`film-nav__item ${isActiveTab === TabValue.Details ? 'film-nav__item--active' : ''}`} onClick={() => setActiveTab(TabValue.Details)}>
+          <li className={`film-nav__item ${activeTab === TabValue.Details ? 'film-nav__item--active' : ''}`} onClick={() => setActiveTab(TabValue.Details)}>
             <Link to='#' className="film-nav__link">Details</Link>
           </li>
-          <li className={`film-nav__item ${isActiveTab === TabValue.Reviews ? 'film-nav__item--active' : ''}`} onClick={() => setActiveTab(TabValue.Reviews)}>
+          <li className={`film-nav__item ${activeTab === TabValue.Reviews ? 'film-nav__item--active' : ''}`} onClick={() => setActiveTab(TabValue.Reviews)}>
             <Link to='#' className="film-nav__link">Reviews</Link>
           </li>
         </ul>
