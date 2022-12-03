04e2959da8bb58af48f615bd0fa04242943d1a9a
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-12-04 00:31:57 +0300

    Добавляет тесты для UserBlock

--- project/src/components/user-block/user-block.test.tsx
+++ project/src/components/user-block/user-block.test.tsx
@@ -4,10 +4,13 @@ import HistoryRouter from '../history-route/history-route';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { Provider } from 'react-redux';
 import UserBlock from './user-block';
-import { AuthorizationStatus } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import thunk from 'redux-thunk';
+import userEvent from '@testing-library/user-event';
+import { Route, Routes } from 'react-router-dom';
 
 const history = createMemoryHistory();
-const mockStore = configureMockStore();
+const mockStore = configureMockStore([thunk]);
 describe('Component: UserBlock', () => {
   it('should render correctly when user is auth', () => {
 
@@ -45,4 +48,57 @@ describe('Component: UserBlock', () => {
 
     expect(screen.getByText(/Sign in/)).toBeInTheDocument();
   });
+
+  it('should dispatch logoutAction when user is auth and click "sign out"', async () => {
+
+    const authorizationStatus = AuthorizationStatus.Auth;
+
+    const store = mockStore({
+      USER: { authorizationStatus: authorizationStatus }
+    });
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <UserBlock />
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    await userEvent.click(screen.getByRole('link'));
+
+    const actions = store.getActions();
+
+    expect(actions[0].type).toBe('user/logout/pending');
+  });
+
+  it('should redirect to AuthScreen when user is noauth and clicks the link', async () => {
+
+    const authorizationStatus = AuthorizationStatus.NoAuth;
+
+    const store = mockStore({
+      USER: { authorizationStatus: authorizationStatus }
+    });
+    render(
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <Routes>
+            <Route
+              path={AppRoute.Main}
+              element={<UserBlock />}
+            />
+            <Route
+              path={AppRoute.SignIn}
+              element={<h1>Auth Screen</h1>}
+            />
+          </Routes>
+          <UserBlock />
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    await userEvent.click(screen.getAllByRole('link')[0]);
+
+    expect(screen.getByText('Auth Screen')).toBeInTheDocument();
+  });
+
 });
